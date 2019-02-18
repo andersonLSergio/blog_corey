20ad6d23edae5e766aba3e66630032e461a6dc37
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-02-18 07:11:11 -0300

    this commit adds password recovery through email functionality

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView, 
-    DetailView, 
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
+
 from .models import Post
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -23,6 +20,16 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # default template: <app> / <model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.conf.global_settings import (EMAIL_BACKEND, EMAIL_HOST,
+                                         EMAIL_HOST_PASSWORD,
+                                         LOGIN_REDIRECT_URL)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +133,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWD')
